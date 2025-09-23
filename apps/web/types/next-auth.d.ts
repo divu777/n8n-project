e488980f182dcbc5379aa14d578e511cfdc1f42f
Divakar Jaiswal <divakarjaiswal707@gmail.com>
2025-09-23 23:48:15 +0530

    Extended the types of the Next auth

---
+++ apps/web/types/next-auth.d.ts
@@ -0,0 +1,22 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    name: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    name: string;
+  }
+}
